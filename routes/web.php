c86da8f36112fbeaa28481f5d758e4413db16b12
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-27 16:31:01 +0200

    added id on comic ph and comments in route

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,30 @@ Route::get('/', function () {
 
 
 // route for my comics
-Route::get('/CardDc', function () {
+Route::get('/TheCardDc', function () {
     $DcCard = config("comics");
 
     return view("CardDc", compact("books"));
 })->name("CardDc");
+
+Route::get('/CardDc/{id}', function($id){
+       $products = config("comics.php");
+
+//     //filtrare i prodotti e recuperare quello che ha lo stesso id
+//     //che voglio ottenere come parametro nell'url
+//     //utilizzare nelle callbackfunction use e $id
+//     array_filter($products, function($product) use ($id) {
+//         return $product["id"] === intval($id);
+        // $keys = array_keys($foundProducts);
+        // var_dump($keys);
+//         var_dump($product);
+//     });
+
+// qui non serve usare use perchè non c'è una callback
+    // foreach ($products as $i => $product){
+    //     if($product["id"] === intval($id)){
+    //         $foundProducts = $product;
+    //         break;
+    //     }
+    // };
+});
